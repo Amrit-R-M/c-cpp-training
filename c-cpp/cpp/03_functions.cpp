2600bbb943cf2d0d4bcd1f29563731b63cfc185c
Amrit Mundlapudi <amritmundlapudi.biz@gmail.com>
2026-09-11 23:06:22 -0400

    C++ fucntions - basic functions, overloading, default parameters

---
+++ c-cpp/cpp/03_functions.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// A function that returns a value
+int add(int a, int b)
+{
+    return a + b;
+}
+
+// An overloaded version - same name, different parameter types
+double add(double a, double b)
+{
+    return a + b;
+}
+
+// A void function - does something but returns nothing
+void greet(string name = "friend")
+{ // default parameter
+    cout << "Hello, " << name << "!" << endl;
+}
+
+// Pass by value - only affects the local copy
+void addOneByValue(int x)
+{
+    x = x + 1;
+}
+
+// Pass by reference - affects the original variable
+void addOneByReference(int &x)
+{
+    x = x + 1;
+}
+
+int main()
+{
+
+    // Calling a basic function
+    int sum = add(5, 3);
+    cout << "Sum: " << sum << endl;
+
+    // Calling the overloaded double version
+    double sumDecimal = add(2.5, 3.5);
+    cout << "Sum decimal: " << sumDecimal << endl;
+
+    // Calling a void function
+    greet("Amrit");
+    greet(); // uses default value "friend"
+
+    return 0;
+}
